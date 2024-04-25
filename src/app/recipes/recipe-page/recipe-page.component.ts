cface63ff2b3cb934184e645e0cb5455453414a4
Nedux <nedasgulb@gmail.com>
2024-04-25 18:02:26 +0300

    Added list of recipes

--- src/app/recipes/recipe-page/recipe-page.component.ts
+++ src/app/recipes/recipe-page/recipe-page.component.ts
@@ -23,4 +23,23 @@ export class RecipePageComponent {
       }
     });
   }
+
+  getHoursOrMinutesFromToday(date: Date | undefined): string {
+    if (date == undefined)
+      return "Some time ago"
+
+    const currentDate = new Date();
+    const diffMilliseconds = currentDate.getTime() - date.getTime();
+    const diffMinutes = Math.floor(diffMilliseconds / (1000 * 60));
+    const diffHours = Math.floor(diffMinutes / 60);
+    const diffDays = Math.floor(diffHours / 24);
+
+    if (diffDays > 0) {
+        return `${diffDays} day${diffDays > 1 ? 's' : ''} ago`;
+    } else if (diffHours > 0) {
+        return `${diffHours} hour${diffHours > 1 ? 's' : ''} ago`;
+    } else {
+        return `${diffMinutes} minute${diffMinutes > 1 ? 's' : ''} ago`;
+    }
+  }
 }

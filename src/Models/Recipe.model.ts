0c237e2f0da9965f2e2185ae943b7b9102c1e87c
Nedux <nedasgulb@gmail.com>
2024-04-25 14:15:26 +0300

    Initial Recipe eco-system - card list of recipes and pages for their details.

---
+++ src/Models/Recipe.model.ts
@@ -0,0 +1,40 @@
+export class Recipe {
+    recipe_id: string;
+    user_id: string;
+    title: string;
+    img_id: string; 
+    subreddit_id: string; 
+    ingredients: string; 
+    description: string;
+    cooking_time: string; 
+    servings: string;
+    date_posted: Date;
+    cooking_difficulty: string;  // TODO: will be an ENUM
+      
+    constructor(
+      recipe_id: string,
+      user_id: string,
+      title: string,
+      img_id: string,
+      subreddit_id: string,
+      ingredients: string,
+      description: string,
+      cooking_time: string,
+      servings: string,
+      date_posted: Date,
+      cooking_difficulty: string
+    ) {
+      this.recipe_id = recipe_id;
+      this.user_id = user_id;
+      this.title = title;
+      this.img_id = img_id;
+      this.subreddit_id = subreddit_id;
+      this.ingredients = ingredients;
+      this.description = description;
+      this.cooking_time = cooking_time;
+      this.servings = servings;
+      this.date_posted = date_posted;
+      this.cooking_difficulty = cooking_difficulty; 
+    }
+  }
+  
---
+++ src/Services/recipes-service.service.ts
@@ -0,0 +1,128 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Recipe } from '../Models/Recipe.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipesService {
+
+  // TODO: Replace with API endpoint
+  // private apiUrl = 'https://api.example.com/recipes';  
+  Recipes_Dummy_List: Recipe[] = [
+    {
+      recipe_id: '1',
+      user_id: '1',
+      title: 'Spaghetti Bolognese',
+      img_id: 'spaghetti_bolognese_image.jpg',
+      subreddit_id: 'cooking',
+      ingredients: 'Spaghetti\nGround beef\nTomato sauce',
+      description: 'Classic spaghetti bolognese recipe with a rich tomato sauce.',
+      cooking_time: '30',
+      servings: '4',
+      date_posted: new Date(),
+      cooking_difficulty: 'Medium'
+    },
+    {
+      recipe_id: '2',
+      user_id: '2',
+      title: 'Chicken Stir-Fry',
+      img_id: 'chicken_stir_fry_image.jpg',
+      subreddit_id: 'recipes',
+      ingredients: 'Chicken breast\nVegetables\nSoy sauce',
+      description: 'Quick and easy chicken stir-fry with fresh vegetables.',
+      cooking_time: '20',
+      servings: '3',
+      date_posted: new Date(),
+      cooking_difficulty: 'Easy'
+    },
+    {
+      recipe_id: '3',
+      user_id: '3',
+      title: 'Vegetarian Chili',
+      img_id: 'vegetarian_chili_image.jpg',
+      subreddit_id: 'vegetarian',
+      ingredients: 'Beans\nTomatoes\nOnions\nPeppers',
+      description: 'Hearty and flavorful vegetarian chili packed with beans and vegetables.',
+      cooking_time: '45',
+      servings: '6',
+      date_posted: new Date(),
+      cooking_difficulty: 'Medium'
+    },
+    {
+      recipe_id: '4',
+      user_id: '2',
+      title: 'Grilled Salmon with Lemon Dill Sauce',
+      img_id: 'grilled_salmon_image.jpg',
+      subreddit_id: 'seafood',
+      ingredients: 'Salmon fillets\nLemon\nFresh dill',
+      description: 'Delicious grilled salmon served with a tangy lemon dill sauce.',
+      cooking_time: '20',
+      servings: '4',
+      date_posted: new Date(),
+      cooking_difficulty: 'Medium'
+    },
+    {
+      recipe_id: '5',
+      user_id: '1',
+      title: 'Mushroom Risotto',
+      img_id: 'mushroom_risotto_image.jpg',
+      subreddit_id: 'italian',
+      ingredients: 'Arborio rice\nMushrooms\nVegetable broth',
+      description: 'Creamy and indulgent mushroom risotto cooked to perfection.',
+      cooking_time: '40',
+      servings: '4',
+      date_posted: new Date(),
+      cooking_difficulty: 'Hard'
+    },
+    {
+      recipe_id: '6',
+      user_id: '3',
+      title: 'Greek Salad',
+      img_id: 'greek_salad_image.jpg',
+      subreddit_id: 'salads',
+      ingredients: 'Cucumbers\nTomatoes\nFeta cheese\nKalamata olives',
+      description: 'Refreshing Greek salad with crisp vegetables and tangy feta cheese.',
+      cooking_time: '15',
+      servings: '2',
+      date_posted: new Date(),
+      cooking_difficulty: 'Easy'
+    },
+    {
+      recipe_id: '7',
+      user_id: '2',
+      title: 'Beef Tacos',
+      img_id: 'beef_tacos_image.jpg',
+      subreddit_id: 'mexican',
+      ingredients: 'Ground beef\nTaco seasoning\nTortillas\nSalsa',
+      description: 'Classic beef tacos with seasoned ground beef and all your favorite toppings.',
+      cooking_time: "25",
+      servings: "4",
+      date_posted: new Date(),
+      cooking_difficulty: 'Easy'
+    },
+    
+  ];
+  
+  // TODO: Uncomment when API will be available
+  // constructor(private http: HttpClient) { }
+  // getRecipes(): Observable<Recipe[]> {
+  //   return this.http.get<Recipe[]>(this.apiUrl);
+  // }
+
+  // getRecipeById(id: number): Observable<Recipe> {
+  //   const url = `${this.apiUrl}/${id}`;
+  //   return this.http.get<Recipe>(url);
+  // }
+  getRecipes(): Recipe[]{
+    return this.Recipes_Dummy_List;
+  }
+
+  getRecipeById(id: String): Recipe | undefined{
+    let item1 = this.Recipes_Dummy_List.find(recipe => recipe.recipe_id === id);
+    
+    return item1;
+  }
+
+}

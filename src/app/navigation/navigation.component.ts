f916c513cde309c4c49ae175471697c18ddd0748
Nedux <nedasgulb@gmail.com>
2024-05-10 14:13:55 +0300

    Added header

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-navigation',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
     styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent{
+    constructor(private router: Router) {}
 
+
+    shouldShowCreateLink(): boolean {
+        return this.router.url !== '/create';
+    }
+    
 }

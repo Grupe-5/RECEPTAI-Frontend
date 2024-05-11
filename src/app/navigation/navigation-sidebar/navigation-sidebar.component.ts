a98fc1b8dc06e4cdaa2c6e341c9663f94c9bb019
Nedux <nedasgulb@gmail.com>
2024-05-11 11:14:32 +0300

    Added side-navigation

---
+++ src/app/navigation/navigation-sidebar/navigation-sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-sidebar',
+  templateUrl: './navigation-sidebar.component.html',
+  styleUrl: './navigation-sidebar.component.scss'
+})
+export class NavigationSidebarComponent {
+
+}

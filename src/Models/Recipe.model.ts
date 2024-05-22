8a32232e932760a9c440821f4d52201f31e054ad
Nedux <nedasgulb@gmail.com>
2024-05-22 14:16:25 +0300

    Recipe create functionality connected to the server

--- src/Models/Recipe.model.ts
+++ src/Models/Recipe.model.ts
@@ -1,43 +1,17 @@
 export class Recipe {
-    recipe_id: string;
-    user_id: string;
-    title: string;
-    img_id: string; 
-    subreddit_id: string; 
-    ingredients: string; 
-    description: string;
-    cooking_time: string; 
-    servings: string;
-    date_posted: Date;
-    cooking_difficulty: string;
-    instructions: string;
-      
-    constructor(
-      recipe_id: string,
-      user_id: string,
-      title: string,
-      img_id: string,
-      subreddit_id: string,
-      ingredients: string,
-      description: string,
-      cooking_time: string,
-      servings: string,
-      date_posted: Date,
-      cooking_difficulty: string,
-      instructions: string,
-    ) {
-      this.recipe_id = recipe_id;
-      this.user_id = user_id;
-      this.title = title;
-      this.img_id = img_id;
-      this.subreddit_id = subreddit_id;
-      this.ingredients = ingredients;
-      this.description = description;
-      this.cooking_time = cooking_time;
-      this.servings = servings;
-      this.date_posted = date_posted;
-      this.cooking_difficulty = cooking_difficulty; 
-      this.instructions = instructions;
-    }
-  }
+    public recipe_id: string;
+    public user_id: string;
+    public title: string;
+    public img_id: string; 
+    public subfoodit_id: number; 
+    public ingredients: string; 
+    public description: string;
+    public cooking_time: string; 
+    public servings: string;
+    public date_posted: Date;
+    public cooking_difficulty: number;
+    public instructions: string;
+
+    constructor(){}  
+}
   
--- src/app/recipes/recipe-create/recipe-create.component.ts
+++ src/app/recipes/recipe-create/recipe-create.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../../Models/Recipe.model';
+import { SubfoodditService } from '../../../Services/subfooddit.service'
+import { RecipesService } from '../../../Services/recipes.service'
 
 @Component({
   selector: 'app-recipe-create',
   templateUrl: './recipe-create.component.html',
   styleUrl: './recipe-create.component.scss'
 })
-export class RecipeCreateComponent {
+export class RecipeCreateComponent {  
+  newRecipe: Recipe = new Recipe();
 
+  constructor(private subfoodditService: SubfoodditService, private recipesService: RecipesService){}
+
+  formSubmited(){
+    // TODO: add error checking 
+    this.newRecipe.subfoodit_id = 1;
+    this.recipesService.postNewRecipe(this.newRecipe)
+  }
 }

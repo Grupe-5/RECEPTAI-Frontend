600b998c6201ba02a191d014e95589b8f91564c2
Afterburn7 <legotronas@gmail.com>
2024-05-31 01:12:51 +0300

    Contact Button
    Added contact button on the navigation bar pre sign in and while signed in. Redirecting to Gmail to contact fooddit support.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -2,50 +2,61 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../../Services/auth.service';
 import { Observable } from 'rxjs';
-import { IUser } from '../../Models/User.model'
-import { SerachBarService } from '../../Services/search-bar.service'
+import { IUser } from '../../Models/User.model';
+import { SerachBarService } from '../../Services/search-bar.service';
 import {OverlayModule} from '@angular/cdk/overlay';
 
 @Component({
-    selector: 'app-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrl: './navigation.component.scss'
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent{
-    status$: Observable<IUser | null>;
-    isInRecipeRoute: Boolean = false;
+export class NavigationComponent {
+  status$: Observable<IUser | null>;
+  isInRecipeRoute: Boolean = false;
 
-    constructor(private router: Router, private authService: AuthService, private serachBarService: SerachBarService) {}
+  constructor(private router: Router, private authService: AuthService, private serachBarService: SerachBarService) {}
 
-    ngOnInit(){
-        this.serachBarService.initSubFoodditNames();
-    }
-    shouldShowSearch(): Boolean{
-        return this.shouldShowRegBtn() && this.shouldShowSignIn();
-    }
+  ngOnInit() {
+    this.serachBarService.initSubFoodditNames();
+  }
 
-    shouldShowRegBtn(): Boolean {
-        return this.router.url !== '/register';
-    }
-    
-    shouldShowUserIcon(): Boolean{
-        return !this.router.url.includes('/user/');
-    }
+  shouldShowSearch(): Boolean {
+    return this.shouldShowRegBtn() && this.shouldShowSignIn();
+  }
 
-    shouldShowSignIn(): Boolean {
-        return this.router.url !== '/sign-in';
-    }
+  shouldShowRegBtn(): Boolean {
+    return this.router.url !== '/register';
+  }
 
-    isLoggedIn(): Boolean{
-        return this.authService.isAuthenticated();
-    }
-    logOut(){
-        this.authService.LogOut();
-        this.router.navigate(['/']);
-    }
+  shouldShowUserIcon(): Boolean {
+    return !this.router.url.includes('/user/');
+  }
 
-    goToUserPage(){
-        this.router.navigate(['user', 'me']);
-    }
-    
+  shouldShowSignIn(): Boolean {
+    return this.router.url !== '/sign-in';
+  }
+
+  shouldShowMailBtn(): Boolean {
+    return true;
+  }
+
+  isLoggedIn(): Boolean {
+    return this.authService.isAuthenticated();
+  }
+
+  logOut() {
+    this.authService.LogOut();
+    this.router.navigate(['/']);
+  }
+
+  goToUserPage() {
+    this.router.navigate(['user', 'me']);
+  }
+
+sendMail() {
+    const mailtoLink = `https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=support@fooddit.com`;
+    window.location.href = mailtoLink;
+}
+
 }

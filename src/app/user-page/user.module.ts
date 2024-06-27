303e56e41560d860b8283a8e4014749473c585f7
Nedux <nedasgulb@gmail.com>
2024-06-27 18:04:14 +0300

    Removed some unnecessary standalone components (trying out module based approach) + Split user pages to separate module and implemented lazy loading

---
+++ src/app/user-page/user.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RecipesCardSmallComponent } from './recipes-card-small/recipes-card-small.component';
+import { UserAccDeleteComponentModalComponent } from './user-acc-delete/user-acc-delete.component';
+import { UserPageComponent } from './user-page.component';
+import { UserRoutingModule } from './user-routing.module'
+import {
+  MatDialogTitle,
+  MatDialogContent,
+  MatDialogActions,
+  MatDialogClose,
+} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  declarations: [
+    RecipesCardSmallComponent,
+    UserPageComponent,
+    UserAccDeleteComponentModalComponent
+  ],
+    
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ]
+})
+export class UserModule { }

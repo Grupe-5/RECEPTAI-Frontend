4f7806ca283d1bbce4db67ab7560e94a7b159bcc
Nedux <nedasgulb@gmail.com>
2024-07-03 08:47:06 +0300

    Some minor change in my reactiveF form approach

--- src/app/authPages/sign-up-page/sign-up-page.component.ts
+++ src/app/authPages/sign-up-page/sign-up-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../../Services/auth.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
@@ -10,70 +10,67 @@ import { ToastrService } from 'ngx-toastr';
   styleUrl: './sign-up-page.component.scss',
 })
 export class SignUpPageComponent {
-  signupForm: FormGroup;
+  signupForm: FormGroup = new FormGroup({
+    username: new FormControl('', {
+      validators: [Validators.required]
+    }),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword:  new FormControl('')
+  });
 
   constructor(
     private router: Router,
-    private fb: FormBuilder,
     private authService: AuthService,
     private toastr: ToastrService
-  ) {
-    this.signupForm = this.fb.group({
-      username: [''],
-      email: [''],
-      password: [''],
-      confirmPassword: [''],
-    });
-  }
+  ) {}
 
   onSignup() {
-    if (this.signupForm.valid) {
-      const { username, email, password, confirmPassword } =
-        this.signupForm.value;
+    const { username, email, password, confirmPassword } =
+      this.signupForm.value;
 
-      let errorMessage: string | null = null;
+    let errorMessage: string | null = null;
 
-      switch (true) {
-        case !username:
-          errorMessage = 'Please provide username!';
-          break;
-        case !email:
-          errorMessage = 'Please provide email!';
-          break;
-        case !password:
-          errorMessage = 'Please provide password!';
-          break;
-        case !confirmPassword:
-          errorMessage = 'Please repeat password!';
-          break;
-        case password !== confirmPassword:
-          errorMessage = "Passwords don't match!";
-          this.signupForm.patchValue({ password: '', confirmPassword: '' });
-          break;
-      }
+    switch (true) {
+      case !username:
+        errorMessage = 'Please provide username!';
+        break;
+      case !email:
+        errorMessage = 'Please provide email!';
+        break;
+      case !password:
+        errorMessage = 'Please provide password!';
+        break;
+      case !confirmPassword:
+        errorMessage = 'Please repeat password!';
+        break;
+      case password !== confirmPassword:
+        errorMessage = "Passwords don't match!";
+        this.signupForm.patchValue({ password: '', confirmPassword: '' });
+        break;
+    }
 
-      if (errorMessage) {
-        this.toastr.error(errorMessage, 'Register Error');
-      } else {
-        this.authService.Signup(username, email, password).subscribe({
-          next: () => {
-            this.router.navigate(['/']);
-          },
-          error: (errorMsg) => {
-            const errorArr = errorMsg.error.errors;
-            if (errorArr !== undefined) {
-              this.toastr.error(
-                errorArr[Object.keys(errorArr)[0]][0],
-                'Register Error'
-              );
-            } else {
-              const errorArr = errorMsg.error;
-              this.toastr.error(errorArr[0].description, 'Register Error');
-            }
-            this.signupForm.patchValue({ password: '', confirmPassword: '' });
+    if (errorMessage) {
+      this.toastr.error(errorMessage, 'Register Error');
+    } else {
+      this.authService.Signup(username, email, password).subscribe({
+        next: () => {
+          this.router.navigate(['/']);
+        },
+        error: (errorMsg) => {
+          const errorArr = errorMsg.error.errors;
+          if (errorArr !== undefined) {
+            this.toastr.error(
+              errorArr[Object.keys(errorArr)[0]][0],
+              'Register Error'
+            );
+          } else {
+            const errorArr = errorMsg.error;
+            this.toastr.error(errorArr[0].description, 'Register Error');
           }
-        });
-      }
+          this.signupForm.patchValue({ password: '', confirmPassword: '' });
+        }
+      });
     }
   }
 }
